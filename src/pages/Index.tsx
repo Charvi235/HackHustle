11b537213160fcf0a6f35b3b3a8bf11942f8cad1
Charvi235 <charvikhandelwal023@gmail.com>
2026-02-19 21:24:21 +0530

    footer update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,37 @@
+// import { HeroSection } from '@/components/home/HeroSection';
+// import { FeaturesSection } from '@/components/home/FeaturesSection';
+// import { StatsSection } from '@/components/home/StatsSection';
+// import { Footer } from "@/components/layout/Footer";
+
+
+// const Index = () => {
+//   return (
+//     <div className="min-h-screen">
+//       <HeroSection />
+//       <FeaturesSection />
+//       <StatsSection />
+//       {/* <Footer /> */}
+//     </div>
+//   );
+// };
+
+// export default Index;
 import { HeroSection } from '@/components/home/HeroSection';
 import { FeaturesSection } from '@/components/home/FeaturesSection';
 import { StatsSection } from '@/components/home/StatsSection';
-import { Footer } from "@/components/layout/Footer";
-//import { CTASection } from '@/components/home/CTASection';
+// Footer yahan se hata diya kyunki wo App.tsx me hai
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <HeroSection />
-      <FeaturesSection />
+      
+      {/* 👇 Yahan id="about" lagaya hai taaki Footer se yahan aa sakein */}
+      <div id="about">
+        <FeaturesSection />
+      </div>
+      
       <StatsSection />
-      {/* <Footer /> */}
     </div>
   );
 };
